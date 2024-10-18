bce74fbb4510635d56d4da74543540adf182dcc3
Artem Voinov <artemvojnov8@gmail.com>
2024-10-18 20:04:18 +0400

    feat(form): added options and parametres to form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,7 +1,18 @@
 import clsx from 'clsx';
 import { useState } from 'react';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
@@ -17,6 +28,36 @@ export const ArticleParamsForm = () => {
 			<aside
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} uppercase={true} weight={800}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamilyOptions[0]}
+						options={fontFamilyOptions}
+						title='цвет шрифта'
+					/>
+					<RadioGroup
+						name='font'
+						options={fontSizeOptions}
+						selected={fontSizeOptions[0]}
+						title='размер шрифта'
+					/>
+					<Select
+						selected={fontColors[0]}
+						options={fontColors}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColors[0]}
+						options={backgroundColors}
+						title='цвет фона'
+					/>
+					<Select
+						selected={contentWidthArr[0]}
+						options={contentWidthArr}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
